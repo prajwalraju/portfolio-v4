08c12677fc34f37c0b7fd9f1a94f76187dbf3250
prajwalraju <prajwalrajup@gmail.com>
2024-01-11 20:17:28 +0530

    fix: navbar cliping and added project asserts

--- components/navBar.js
+++ components/navBar.js
@@ -8,12 +8,14 @@ function NavBar() {
   return (
     <div className="navBar flex w-full flex-col md:flex-row lg:flex-row xl:flex-row 2xl:flex-row gap-2 ">
       <Image className="logo" alt="Logo" width={40} src={Logo} />
-      <div className="navMenu w-full  flex gap-2 overflow-scroll justify-center items-center">
-        {Object.entries(data.navBarData).map(([key, value]) => (
-          <Link href={value.link} key={key}>
-            <p className="link"> {value.name}</p>
-          </Link>
-        ))}
+      <div className="justify-center flex w-full">
+        <div className="navMenu flex gap-2 overflow-scroll items-center">
+          {Object.entries(data.navBarData).map(([key, value]) => (
+            <Link href={value.link} key={key}>
+              <p className="link"> {value.name}</p>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
