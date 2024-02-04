32020e89a6b0b0a8db304369da4431ea8c29a8a1
prajwalraju <prajwalrajup@gmail.com>
2024-02-05 00:53:00 +0530

    feat: add splash screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,37 @@
+<script context="module">
+  export const prerender = true;
+</script>
+
 <script>
+  import { onMount } from "svelte";
   import ContactMe from "./ContactMe.svelte";
   import Header from "./Header.svelte";
+  import SplashScreen from "./SplashScreen.svelte";
   import "./styles.css";
   import "animate.css";
   export let data;
+
+  let showSplash = true;
+
+  onMount(() => {
+    setTimeout(() => {
+      showSplash = false;
+    }, 800);
+  });
 </script>
 
-<div class="app flex flex-col p-3 lg:p-10">
-  <Header />
+{#if showSplash}
+  <SplashScreen />
+{:else}
+  <div class="app flex flex-col p-3 lg:p-10">
+    <Header />
 
-  <main>
-    <slot />
-  </main>
+    <main>
+      <slot />
+    </main>
 
-  <footer>
-    <ContactMe contactDetails={data.data.contactDetails} />
-  </footer>
-</div>
+    <footer>
+      <ContactMe contactDetails={data.data.contactDetails} />
+    </footer>
+  </div>
+{/if}
---
+++ src/routes/SplashScreen.svelte
@@ -0,0 +1,11 @@
+<script>
+  import logo from "$lib/images/SplashLogo.svg";
+</script>
+
+<div class="flex items-center justify-center h-screen">
+  <img
+    class="splashScreenLogo w-[10rem] h-fit animate__animated animate__fadeIn"
+    alt=""
+    src={logo}
+  />
+</div>
